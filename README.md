b95dd3ea9753f8d40852d7dd7b9c24efdf93de2c
Patrick Brown <patrick.brown@blyncsy.com>
2019-10-18 15:25:27 -0600

    readme, working lightning, green red not working

---
+++ README.md
@@ -0,0 +1,4 @@
+# Halloween
+## LEDs
+
+https://github.com/adafruit/Adafruit_Python_WS2801/blob/master/Adafruit_WS2801/WS2801.py
--- halloween.py
+++ halloween.py
@@ -10,6 +10,7 @@ import SocketServer
 
 import threading
 import sys
+import random
 
 # Configure the count of pixels:
 PIXEL_COUNT = 50
@@ -22,14 +23,59 @@ UPDATES_PER_SECOND = 50
 
 PORT = 9067
 
+FLASHES_MAX = 8
+LIGHTNING_PROB = 0.1
+
 pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
 
-def lightning(pixels):
-    pixels.clear()
-    pixels.show()
+class Lightning:
+    def __init__(self):
+        self.flash = 0
+        self.ledStart = 0
+        self.ledLen = 0
+        self.delay = 0
+        self.last = 0
+
+    def fadeToBlackBy(self, amount):
+        for i in range(PIXEL_COUNT):
+            color = pixels.get_pixel_rgb(i)
+            pixels.set_pixel_rgb(i, max(0, color[0] - amount), max(0, color[1] - amount), max(0, color[2] - amount))
+        pixels.show()
+
+    def run(self):
+        self.fadeToBlackBy(10)
+
+        dimmer = random.randrange(1, 3)
+
+        if self.flash == 0:
+            if random.random() < LIGHTNING_PROB:
+                self.flash = random.randrange(3, FLASHES_MAX)
+                self.ledStart = random.randrange(PIXEL_COUNT)
+                self.ledLen = random.randrange(PIXEL_COUNT - self.ledStart)
+                self.last = time.time() * 1000
+                self.delay = -2
+                dimmer = 5
+
+        if self.flash > 0 and self.last + self.delay < time.time() * 1000:
+            #Flash
+            for i in range(self.ledStart, self.ledStart + self.ledLen):
+                pixels.set_pixel_rgb(i, 255 / dimmer, 255 / dimmer, 255 / dimmer)
+            pixels.show()
+            time.sleep(random.randrange(4,10) / 1000.0)
+            for i in range(self.ledStart, self.ledStart + self.ledLen):
+                pixels.set_pixel_rgb(i, 0, 0, 0)
+            pixels.show()
+
+            if (dimmer == 5):
+                self.delay = 150
+            else:
+                self.delay = random.randrange(50,150)
+
+            self.last = time.time() * 1000
+            self.flash -= 1
 
 global state
-state = 999
+state = 0
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
@@ -39,28 +85,29 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        print(self.path)
+        global state
         if self.path == "/green":
-            global state
             state = 0
             self.goHome()
         if self.path == "/red":
-            global state
             state = 1
             self.goHome()
         if self.path == "/lightning":
-            global state
             state = 2
             self.goHome()
         if self.path == "/clear":
-            global state
             state = 999
             self.goHome()
         else:
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
+def flow(i):
+    return ((i * 2 + int(time.time() * 20)) % 200) + 56
+
 if __name__ == "__main__":
     Handler = ServerHandler
+
+    lightning = Lightning()
 
     httpd = SocketServer.TCPServer(("", PORT), Handler)
     thread = threading.Thread(target = httpd.serve_forever)
@@ -70,14 +117,16 @@ if __name__ == "__main__":
         while True:
             if state == 0:
                 # Green
-                pixels.set_pixels(Adafruit_WS2801.RGB_to_color(0, 255, 0))
+                for i in range(PIXEL_COUNT):
+                    pixels.set_pixel_rgb(i, 0, flow(i), 0)
                 pixels.show()
             if state == 1:
                 # Red
-                pixels.set_pixels(Adafruit_WS2801.RGB_to_color(255, 0, 0))
+                for i in range(PIXEL_COUNT):
+                    pixels.set_pixel_rgb(i, flow(i), 0, 0)
                 pixels.show()
             if state == 2:
-                lightning(pixels)
+                lightning.run()
             if state == 999:
                 pixels.clear()
                 pixels.show()
